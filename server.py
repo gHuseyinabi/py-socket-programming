2176c4fc4424c8a63c9f96c277a3ec074aaf0319
gHuseyinabi <68753989+gHuseyinabi@users.noreply.github.com>
2021-01-27 21:13:50 +0300

    Update server.py
    mesaj almayı ve vermeyi async yapar (test etme imkanım olmadı)
    bu mesaj almak için mesaj yazma zorunluluğunu kaldırır

--- server.py
+++ server.py
@@ -1,21 +1,24 @@
-anahtar = 1
-
 import socket
+import asyncio
+from concurrent.futures import ThreadPoolExecutor
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 s.bind(("127.0.0.1", 1234))
 s.listen(5)
+async def ainput(prompt: str = "") -> str:
+    with ThreadPoolExecutor(1, "AsyncInput") as executor:
+        return await asyncio.get_event_loop().run_in_executor(executor, input, prompt)      #shoutout to delivrance
 
-while True:
-    clientsocket, address = s.accept()
-    print(f"Connection from {address} has been established!")
-    while anahtar == 1:
-     message = input("What is your message to server? : ")
-     clientsocket.send(bytes(message, "utf-8"))
-     print("Your message is succesfully sended!")
-     msg = s.recv(1024)
-     print(msg.decode("utf-8"))
-     continue
-
-
+async def getmessageandpost():
+    clientsocket.send(bytes(await ainput("Message:"),"utf-8")
+    print("Your message is succesfully sended!")
+async def recievemessages():
+    while msg = s.recv(1024) and msg is not None:
+        print(msg.decode("utf-8"))                  
+async def main():
+        clientsocket, address = s.accept()
+        print(f"Connection from {address} has been established!")
+        while True:
+            await getmessageandpost()
+            await recievemessages()
+asyncio.run(main())
